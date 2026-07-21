5329f446be97bb48e2aa33e23c0fe66b524ffd08
chocoandmilktea <choco.and.milktea.gj@gmail.com>
2026-07-21 23:46:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var watcher = require("./watcher");
+var webapi = require("./webapi");
 
 process.on("unhandledRejection", function (err) {
   console.error("[fatal] unhandledRejection:", err);
 });
 
 watcher.start();
+webapi.start();
